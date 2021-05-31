c8ed5b6beaae1ad68e95e20ec8c88cde8cd9a567
sehwan505 <sehwan505@gmail.com>
2021-05-31 11:56:49 +0900

    klqwenmnads

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -177,8 +177,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database-23cluster',
+		'USER' : 'sehwan505',
+		'PASSWORD' : 'sh904217',
+		'HOST' : 'database-23cluster.c9utwqgrlc8e.ap-northeast-2.rds.amazonaws.com',
+		'PORT' : '3306',
+		'OPTIONS' : {
+			'init_command' : 'SET sql_mode="STRICT_TRANS_TABLES"'
+		}
     }
 }
 
